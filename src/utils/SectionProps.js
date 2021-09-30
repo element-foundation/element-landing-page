b45c9a621df591455652644ee75931bd6b2f592d
tinahaibodi <27844500+tinahaibodi@users.noreply.github.com>
2021-09-30 04:28:39 -0400

    adding babel

--- src/utils/SectionProps.js
+++ src/utils/SectionProps.js
@@ -1,61 +1,70 @@
-import PropTypes from 'prop-types';
-
 // Props shared by all sections
 const SectionShared = {
-  types: {
-    topOuterDivider: PropTypes.bool,
-    bottomOuterDivider: PropTypes.bool,    
-    topDivider: PropTypes.bool,
-    bottomDivider: PropTypes.bool,
-    hasBgColor: PropTypes.bool,
-    invertColor: PropTypes.bool
-  },
-  defaults: {
-    topOuterDivider: false,
-    bottomOuterDivider: false,    
-    topDivider: false,
-    bottomDivider: false,
-    hasBgColor: false,
-    invertColor: false
+  props: {
+    topOuterDivider: {
+      type: Boolean,
+      default: false
+    },
+    bottomOuterDivider: {
+      type: Boolean,
+      default: false
+    },    
+    topDivider: {
+      type: Boolean,
+      default: false
+    },
+    bottomDivider: {
+      type: Boolean,
+      default: false
+    },
+    hasBgColor: {
+      type: Boolean,
+      default: false
+    },
+    invertColor: {
+      type: Boolean,
+      default: false
+    }
   }
-}
+};
 
 // Default section props
 export const SectionProps = {
-  types: {
-    ...SectionShared.types
-  },
-  defaults: {
-    ...SectionShared.defaults
+  props: {
+    ...SectionShared.props
   }
-}
+};
 
 // Section split props
 export const SectionSplitProps = {
-  types: {
-    ...SectionShared.types,
-    invertMobile: PropTypes.bool,
-    invertDesktop: PropTypes.bool,
-    alignTop: PropTypes.bool,
-    imageFill: PropTypes.bool
-  },
-  defaults: {
-    ...SectionShared.defaults,
-    invertMobile: false,
-    invertDesktop: false,
-    alignTop: false,
-    imageFill: false
+  props: {
+    ...SectionShared.props,
+    invertMobile: {
+      type: Boolean,
+      default: false
+    },
+    invertDesktop: {
+      type: Boolean,
+      default: false
+    },
+    alignTop: {
+      type: Boolean,
+      default: false
+    },
+    imageFill: {
+      type: Boolean,
+      default: false
+    }
   }
-}
+};
 
 // Section tiles props
 export const SectionTilesProps = {
-  types: {
-    ...SectionShared.types,
-    pushLeft: PropTypes.bool
-  },
-  defaults: {
-    ...SectionShared.defaults,
-    pushLeft: false
+  props: {
+    ...SectionShared.props,
+    pushLeft: {
+      type: Boolean,
+      default: false
+    }
   }
-}
+};
