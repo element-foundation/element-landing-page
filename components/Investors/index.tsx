9d8ad0ba674928a137fc1f1c0aa3d468710c04b8
tinahaibodi <27844500+tinahaibodi@users.noreply.github.com>
2021-10-04 15:09:10 -0400

    implementing grid for investors and adding investors svgs

--- components/Investors/index.tsx
+++ components/Investors/index.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import { SectionContainer } from "components/common/Elements/Container";
-import { InvestorsWrapper } from "components/Investors/styles";
+import {
+  InvestorsWrapper,
+  GridContainer,
+  SmallScreen,
+  LargeScreen,
+} from "components/Investors/styles";
 import {
   AbsoluteSVGContainer,
   SectionTitle,
 } from "components/GrowSavings/styles";
 
 import QuarterO from "public/assets/png/o.png";
+import Logo from "public/assets/svg/Logo.svg";
+import a_capital from "public/assets/svg/a_capital.svg";
+import balancer from "public/assets/svg/balancer.svg";
+import robot_ventures from "public/assets/svg/robot_ventures.svg";
+import consensys from "public/assets/svg/consensys.svg";
 
 export const Investors = () => {
   return (
@@ -16,10 +26,32 @@ export const Investors = () => {
       </AbsoluteSVGContainer>
       <SectionContainer gridColumn="1/13" textAlign="center" padding="0">
         <SectionTitle variant="primary">Our investors</SectionTitle>
+        <LargeScreen>
+          <GridContainer gridTemplateColumns="repeat(1, 1fr)">
+            <Image src={Logo} alt="Logo of Logo" />
+          </GridContainer>
+          <GridContainer gridTemplateColumns="repeat(2, 1fr)">
+            <Image src={a_capital} alt="Logo of a_capital" />
+            <Image src={Logo} alt="Logo of Logo" />
+          </GridContainer>
+          <GridContainer gridTemplateColumns="repeat(3, 1fr)">
+            <Image src={balancer} alt="Logo of Balancer" />
+            <Image src={robot_ventures} alt="Logo of ROBOT VENTURES" />
+            <Image src={consensys} alt="Logo of Consensys" />
+          </GridContainer>
+          <AbsoluteSVGContainer right="-244px" bottom="-244px">
+            <Image src={QuarterO} alt="SVG Illustration" />
+          </AbsoluteSVGContainer>
+        </LargeScreen>
+        <SmallScreen>
+          <GridContainer gridTemplateColumns="repeat(3, 1fr)">
+            <Image src={a_capital} alt="Logo of a_capital" />
+            <Image src={balancer} alt="Logo of Balancer" />
+            <Image src={robot_ventures} alt="Logo of ROBOT VENTURES" />
+            <Image src={consensys} alt="Logo of Consensys" />
+          </GridContainer>
+        </SmallScreen>
       </SectionContainer>
-      <AbsoluteSVGContainer right="-250px" bottom="-244px">
-        <Image src={QuarterO} alt="ax" />
-      </AbsoluteSVGContainer>
     </InvestorsWrapper>
   );
 };
--- components/Investors/styles.ts
+++ components/Investors/styles.ts
@@ -3,8 +3,12 @@ import styled from "styled-components";
 
 export const InvestorsWrapper = styled.div`
   position: relative;
-  padding: 18rem 0;
+  padding: 18rem 0 0 0;
   overflow: hidden;
+
+  h3 {
+    padding-bottom: 4rem;
+  }
 
   .decorative-line {
     width: 100%;
@@ -34,7 +38,7 @@ export const InvestorsWrapper = styled.div`
   }
 
   @media ${devices.tabletL} {
-    padding: 12rem 0;
+    padding: 12rem 0 0 0;
   }
 
   @media ${devices.tabletM} {
@@ -42,6 +46,58 @@ export const InvestorsWrapper = styled.div`
   }
 
   @media ${devices.mobileL} {
-    padding: 5rem 0 0;
+    padding: 5rem 0 0 0;
+  }
+`;
+
+export const GridContainer = styled.div<any>`
+  display: grid;
+  grid-template-columns: ${({ gridTemplateColumns }) => gridTemplateColumns};
+  grid-gap: 5rem 5rem;
+  gap: 5rem 5rem;
+  text-align: start;
+  max-width: 70rem;
+  margin: 4rem auto 0 auto;
+  align-items: center;
+
+  @media ${devices.desktopM} {
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 5rem 3rem;
+    gap: 5rem 3rem;
+  }
+`;
+
+export const LargeScreen = styled.div`
+  display: block;
+
+  @media ${devices.tabletM} {
+    display: none;
+  }
+`;
+
+export const SmallScreen = styled.div`
+  display: none;
+
+  ${GridContainer} {
+    @media ${devices.desktopM} {
+      grid-template-columns: repeat(2, 1fr);
+      grid-gap: 5rem 3rem;
+      gap: 5rem 3rem;
+    }
+
+    @media ${devices.mobileL} {
+      grid-template-columns: repeat(1, 1fr);
+      grid-gap: 5rem 3rem;
+      gap: 5rem 3rem;
+
+      img {
+        width: 60%;
+      }
+    }
+  }
+
+  @media ${devices.tabletM} {
+    position: relative;
+    display: block;
   }
 `;
