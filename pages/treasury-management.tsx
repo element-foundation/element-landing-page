429fba1121fa39d4f65c6831d5a91ef7851683ce
tinahaibodi <27844500+tinahaibodi@users.noreply.github.com>
2021-12-10 14:27:41 -0500

    changing button links

--- pages/treasury-management.tsx
+++ pages/treasury-management.tsx
@@ -11,7 +11,7 @@ const TreasuryManagement = () => (
       title="Element Treasury Management"
       text="Element is the perfect treasury diversification solution allowing Protocols, DAOs, and organizations to earn fixed rate yield on treasury capital while maintaining the ability to exit if needed."
       buttonText="Learn more"
-      buttonHref="https://app.element.fi/"
+      buttonHref="https://app.element.fi/fixedrates"
       hasArrow
       darkLottie={VaultAnimationDark}
       lightLottie={VaultAnimationDark}
@@ -20,7 +20,7 @@ const TreasuryManagement = () => (
       title="Grow Your Capital"
       text="Element’s capital flexibility enables organizations or DAO treasuries a way to get a fixed or variable rate exposure on their capital depending on their risk profile and allocation strategy."
       buttonText="Learn more about our fixed rates"
-      buttonHref="https://github.com/element-fi/elf-sdk"
+      buttonHref="https://medium.com/element-finance/fixed-rate-interest-markets-a-casual-users-journey-through-fixed-rate-interest-using-element-50f420df1859"
       hasArrow
       largeTitle
       // darkLottie={}
@@ -30,7 +30,7 @@ const TreasuryManagement = () => (
       title="Onboarding Support"
       text="We actively speak with you about how best to leverage Element for your treasury management needs. When interested, we walk you through the platform, discuss options, and even support you through the deposit process. If you’re interested in using Element, please reach out to us!"
       buttonText="Contact us"
-      buttonHref="https://app.element.fi/fixedrates/"
+      buttonHref="https://docs.google.com/forms/d/e/1FAIpQLSd0y1RrxWy5UNZZ3Pfoeb7CWqb2_svgAngqBQKstGaeE1OMGg/viewform?usp=send_form"
       hasArrow
       largeTitle
       // darkLottie={}
