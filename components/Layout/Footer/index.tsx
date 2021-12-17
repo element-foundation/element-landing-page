7fe1b71f59f8fd664eb77b49bf88460a0683c281
tinahaibodi <27844500+tinahaibodi@users.noreply.github.com>
2021-12-17 15:51:20 -0500

    adding ToS and pp

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -110,6 +110,12 @@ export const Footer = () => {
               <ExternalLink href="https://www.youtube.com/channel/UCwxX4xrw-AZF_7dB7gVMaSw">
                 YouTube
               </ExternalLink>
+              <ExternalLink href="https://elementfi.s3.us-east-2.amazonaws.com/element-finance-terms-of-service.pdf">
+                Terms of Service
+              </ExternalLink>
+              <ExternalLink href="https://elementfi.s3.us-east-2.amazonaws.com/element-finance-privacy-policy.pdf">
+                Privacy Policy
+              </ExternalLink>
             </LinkContainer>
             <LinkContainer darkTheme={resolvedTheme === "dark"}>
               <h3>Change theme</h3>
